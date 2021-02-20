d0bb8764feb735ece3c53c8e064c8c7e166979ac
anonymous-OO7 <gauravyadav00729@gmail.com>
2021-02-21 00:20:42 +0530

    Day-12 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:helloflutter/models/catalog.dart';
 import 'package:helloflutter/widgets/drawer.dart';
+import 'package:helloflutter/widgets/item_widget.dart';
+
 class Homepage extends StatelessWidget {
   final int days = 30;
   final String name = "Gaurav";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Catalog App")
-        ),
-        body: Center(
-          child: Container(
-            child: Text("welcome to $days days of flutter by $name"),
+        appBar: AppBar(title: Text("Catalog App")),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            },
           ),
         ),
-        drawer: MyDrawer ()
-        
-        );
+        drawer: MyDrawer());
   }
 }
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,20 @@
+class CatalogModel{
+ static final items = [
+  Item(
+    id: 1,
+    name: "iphone 12 pro",
+    desc: "apple i phone 12th generation",
+    price: 999,
+    color: "#33505a",
+    image:
+        "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020.jpg.og.jpg?202102050019",)
+];
+
+
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -9,14 +24,3 @@ class Item {
   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
 
-final products = [
-  Item(
-    id: "Gaurav",
-    name: "iphone 12 pro",
-    desc: "apple i phone 12th generation",
-    price: 999,
-    color: "#33505a",
-    image:
-        "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020.jpg.og.jpg?202102050019",
-  )
-];
